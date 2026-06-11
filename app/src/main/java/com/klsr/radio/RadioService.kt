943c8aff397816f60c22cec8b78f175876375b64
ogunfoworakehinde <ogunfoworakehinde55@gmail.com>
2026-06-11 13:11:16 +0100

    Fix RadioService callback methods

--- app/src/main/java/com/klsr/radio/RadioService.kt
+++ app/src/main/java/com/klsr/radio/RadioService.kt
@@ -6,10 +6,8 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
-import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.IBinder
-import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
@@ -40,13 +38,7 @@ class RadioService : Service() {
         super.onCreate()
         createChannel()
         player = ExoPlayer.Builder(this).build()
-        mediaSession = MediaSession.Builder(this, player)
-            .setCallback(object : MediaSession.Callback {
-                override fun onPlayRequest() { player.play() }
-                override fun onPauseRequest() { player.pause() }
-                override fun onStopRequest() { stopSelf() }
-            })
-            .build()
+        mediaSession = MediaSession.Builder(this, player).build()  // no custom callback needed
         playStation(stationIndex)
     }
 
@@ -98,7 +90,6 @@ class RadioService : Service() {
         val openIntent = PendingIntent.getActivity(this, 0,
             Intent(this, MainActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
 
-        // Play/Pause action
         val ppIcon = if (player.isPlaying) android.R.drawable.ic_media_pause else android.R.drawable.ic_media_play
         val ppAction = NotificationCompat.Action(ppIcon,
             if (player.isPlaying) "Pause" else "Play",
@@ -106,7 +97,6 @@ class RadioService : Service() {
                 Intent(this, RadioService::class.java).apply { action = ACTION_PLAY_PAUSE },
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE))
 
-        // Cancel (stop) action with ✖
         val stopAction = NotificationCompat.Action(
             android.R.drawable.ic_menu_close_clear_cancel, "Cancel",
             PendingIntent.getService(this, 1,
